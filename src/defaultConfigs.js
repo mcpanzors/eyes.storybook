bb272b776fafbcc65c716fbce5253209f75d5774
Oleh Astappiev <astappev@gmail.com>
2017-09-05 16:47:48 +0200

    Add default viewportSize

--- src/defaultConfigs.js
+++ src/defaultConfigs.js
@@ -1,7 +1,9 @@
 module.exports = {
     apiKey: process.env.APPLITOOLS_API_KEY,
     appName: null,
-    viewportSize: null, // can be array of objects or single object, e.g. {width: 800, height: 600}
+    viewportSize: [ // can be array of objects or single object, e.g. {width: 800, height: 600}
+        {width: 800, height: 600}
+    ],
 
     storybookAddress: null, // if null, then we will try to run storybook server using settings below
     storybookPort: 9001,
